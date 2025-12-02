7ca1a28f2b31be9d7a6d74659b969988186b9af3
Tristan-Kainama <io.kainama@gmail.com>
2025-12-02 17:26:22 +0700

    Completed exercises 6 to 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splittedString = string.split("");
+    let result = "";
+    for (let i = splittedString.length; i > 0; i--) {
+        result += splittedString[i-1];
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num1 % 1 !== 0 || num2 % 1 !== 0) return "ERROR";
+    if (Number(num1) !== num1 || Number(num2) !== num2) return "ERROR";
 
+    let result = 0;
+    if (num1 < num2) {
+        for(let i = num1; i <= num2; i++) {
+            result += i;
+        }
+        return result;
+    } else if (num1 > num2) {
+        for(let i = num2; i <= num1; i++) {
+            result += i;
+        }
+        return result;
+    } else {
+        return num1;
+    }
 };
 
 // Do not edit below this line
